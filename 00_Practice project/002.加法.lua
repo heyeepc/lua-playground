26df09aa8db4f648dc57db5b51db87866f7b1cd4
heyeepc <2675458141@qq.com>
2025-10-30 01:49:46 +0800

    Update 002.加法.lua

--- 00_Practice project/002.加法.lua
+++ 00_Practice project/002.加法.lua
@@ -6,3 +6,10 @@ local sum = number1 + number2
 
 -- 打印结果
 print("和是: " .. sum) -- 输出: 和是: 15
+
+
+local a = "20"  -- 这是一个字符串
+local b = 3     -- 这是一个数字
+
+local result = a + b
+print("字符串和数字的加法结果: " .. result) -- 输出: 字符串和数字的加法结果: 23
